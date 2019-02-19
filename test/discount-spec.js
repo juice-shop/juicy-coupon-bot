e185350bad0433ea0c717e6290c799f0e353b57c
Bjoern Kimminich <bjoern.kimminich@kuehne-nagel.com>
2019-02-19 13:30:12 +0100

    Use deploy config over after_script

--- test/discount-spec.js
+++ test/discount-spec.js
@@ -4,7 +4,7 @@ const randomDiscount = require('../lib/discount')
 
 describe('Discount', () => {
   it('should be between 10% and 40%', () => {
-    for (let i=0; i<100; i++) {
+    for (let i = 0; i < 100; i++) {
       expect(randomDiscount() >= 10).to.equal(true)
       expect(randomDiscount() <= 40).to.equal(true)
     }
