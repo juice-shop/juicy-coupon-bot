71ce34d2b107cc1053287946b67cb42367220b77
Bjoern Kimminich <bjoern.kimminich@kuehne-nagel.com>
2019-02-21 11:15:02 +0100

    Fix code linter errros

--- test/statusText-spec.js
+++ test/statusText-spec.js
@@ -4,20 +4,20 @@ const statusText = require('../lib/statusText')
 
 describe('Status Text', () => {
   it('should always include robot emoji', () => {
-    for (let i=0; i<100; i++) {
+    for (let i = 0; i < 100; i++) {
       expect(statusText()).to.include('[🤖]')
     }
   })
 
   it('should always include #coupon hashtag', () => {
-    for (let i=0; i<100; i++) {
+    for (let i = 0; i < 100; i++) {
       expect(statusText()).to.include('#coupon')
     }
   })
 
   it('should always mention discount amount', () => {
-    for (let i=10; i<40; i++) {
-      expect(statusText(i)).to.include(i+'%')
+    for (let i = 10; i < 40; i++) {
+      expect(statusText(i)).to.include(i + '%')
     }
   })
 
