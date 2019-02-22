6a4425f6d347c28f938ace49a5c690e0810b3a47
Bjoern Kimminich <bjoern.kimminich@kuehne-nagel.com>
2019-02-22 16:24:37 +0100

    Add automatic Reddit comment publishing

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ const randomDiscount = require('./lib/randomDiscount')
 const currentCoupons = require('./lib/currentCoupons')
 const statusText = require('./lib/statusText')
 const publishTweet = require('./lib/publishTweet')
+const publishReddit = require('./lib/publishReddit')
 
 module.exports = () => {
   const { expiryDate, discountCodes } = currentCoupons()
   const discount = randomDiscount()
   const coupon = discountCodes[discount + '%']
 
-  publishTweet(statusText(discount, coupon, expiryDate))
+  const status = statusText(discount, coupon, expiryDate)
+
+  publishTweet(status)
+  publishReddit(status)
 }
