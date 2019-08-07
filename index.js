e93d16324cd2f07a081474240e09f0948ffe5dc9
Bjoern Kimminich <bjoern.kimminich@kuehne-nagel.com>
2019-08-07 16:16:56 +0200

    Add posts on Facebook page of OWASP Juice Shop
    (waiting for AppReview from Facebook)

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const currentCoupons = require('./lib/currentCoupons')
 const twitterStatus = require('./lib/twitterStatus')
 const redditComment = require('./lib/redditComment')
 const publishTweet = require('./lib/publishTweet')
+const publishFacebook = require('./lib/publishFacebook')
 const publishReddit = require('./lib/publishReddit')
 
 module.exports = () => {
@@ -11,5 +12,6 @@ module.exports = () => {
   const coupon = discountCodes[discount + '%']
 
   publishTweet(twitterStatus(discount, coupon, expiryDate))
+  publishFacebook(twitterStatus(discount, coupon, expiryDate)) // TODO Customize Facebook posts instead of using Twitter status layout
   publishReddit(redditComment(discount, coupon, expiryDate))
 }
