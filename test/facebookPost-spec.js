8afcbfc53fd50e32fe74fcf3171092e6d954c1f9
Bjoern Kimminich <bjoern.kimminich@gmx.de>
2019-08-09 01:42:07 +0200

    Actually run Facebook post tests instead of Twitter twice

--- test/facebookPost-spec.js
+++ test/facebookPost-spec.js
@@ -1,41 +1,41 @@
 const chai = require('chai')
 const expect = chai.expect
-const twitterStatus = require('../lib/twitterStatus')
+const facebookPost = require('../lib/facebookPost')
 
-describe('Twitter status', () => {
+describe('Facebook post', () => {
   it('should always include robot emoji', () => {
     for (let i = 0; i < 100; i++) {
-      expect(twitterStatus()).to.include('🤖')
+      expect(facebookPost()).to.include('🤖')
     }
   })
 
   it('should always include #coupon hashtag', () => {
     for (let i = 0; i < 100; i++) {
-      expect(twitterStatus()).to.include('#coupon')
+      expect(facebookPost()).to.include('#coupon')
     }
   })
 
   it('should always mention discount amount', () => {
     for (let i = 10; i < 40; i++) {
-      expect(twitterStatus(i)).to.include(i + '%')
+      expect(facebookPost(i)).to.include(i + '%')
     }
   })
 
   it('should always contain coupon code', () => {
     for (let i = 0; i < 100; i++) {
-      expect(twitterStatus(undefined, ':COUPON:')).to.include(':COUPON:')
+      expect(facebookPost(undefined, ':COUPON:')).to.include(':COUPON:')
     }
   })
 
   it('should always mention expiration date', () => {
     for (let i = 0; i < 100; i++) {
-      expect(twitterStatus(undefined, undefined, ':EXPIRATION:')).to.include(':EXPIRATION:')
+      expect(facebookPost(undefined, undefined, ':EXPIRATION:')).to.include(':EXPIRATION:')
     }
   })
 
   it('should never be longer than 200 characters', () => {
     for (let i = 0; i < 100; i++) {
-      expect(twitterStatus().length <= 200).to.equal(true)
+      expect(facebookPost().length <= 200).to.equal(true)
     }
   })
 })
