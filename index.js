50fccc753d34631daea1d8b31a9b4ead424856df
Bjoern Kimminich <bjoern.kimminich@gmx.de>
2020-12-15 01:04:15 +0100

    Turn on only Twitter publishing
    (due to access token issues on Facebook and recurring archiving of Reddit
     posts to put coupons as comments into)

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const randomDiscount = require('./lib/randomDiscount')
 const currentCoupons = require('./lib/currentCoupons')
 const twitterStatus = require('./lib/twitterStatus')
-const facebookPost = require('./lib/facebookPost')
-const redditComment = require('./lib/redditComment')
+// const facebookPost = require('./lib/facebookPost')
+// const redditComment = require('./lib/redditComment')
 const publishTweet = require('./lib/publishTweet')
-const publishFacebook = require('./lib/publishFacebook')
-const publishReddit = require('./lib/publishReddit')
+// const publishFacebook = require('./lib/publishFacebook')
+// const publishReddit = require('./lib/publishReddit')
 
 module.exports = () => {
   const { expiryDate, discountCodes } = currentCoupons()
@@ -13,6 +13,6 @@ module.exports = () => {
   const coupon = discountCodes[discount + '%']
 
   publishTweet(twitterStatus(discount, coupon, expiryDate))
-  publishFacebook(facebookPost(discount, coupon, expiryDate))
-  publishReddit(redditComment(discount, coupon, expiryDate))
+  // publishFacebook(facebookPost(discount, coupon, expiryDate))
+  // publishReddit(redditComment(discount, coupon, expiryDate))
 }
