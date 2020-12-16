927cd711d8c9d9b05dee3b0df9410ea4b97b4070
Bjoern Kimminich <bjoern.kimminich@gmx.de>
2020-12-16 19:53:47 +0100

    Publish Reddit coupon with proper title

--- index.js
+++ index.js
@@ -14,5 +14,5 @@ module.exports = () => {
 
   publishTweet(twitterStatus(discount, coupon, expiryDate))
   publishFacebook(facebookPost(discount, coupon, expiryDate))
-  publishReddit(redditComment(discount, coupon, expiryDate))
+  publishReddit(redditComment(discount, coupon, expiryDate), `New coupon code (valid until ${expiryDate})`)
 }
