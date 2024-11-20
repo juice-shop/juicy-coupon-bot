64a50a1735c8d922bdc29a52c862bec82a1055fe
badboi-s <bansalabhinav751@gmail.com>
2024-11-20 13:27:01 +0530

    add publishBlueSky.js file

--- index.js
+++ index.js
@@ -5,19 +5,23 @@
 
 const randomDiscount = require('./lib/randomDiscount')
 const currentCoupons = require('./lib/currentCoupons')
-const twitterStatus = require('./lib/twitterStatus')
-const facebookPost = require('./lib/facebookPost')
+// const twitterStatus = require('./lib/twitterStatus')
+// const facebookPost = require('./lib/facebookPost')
 const redditPost = require('./lib/redditPost')
-const publishTweet = require('./lib/publishTweet')
-const publishFacebook = require('./lib/publishFacebook')
+// const publishTweet = require('./lib/publishTweet')
+// const publishFacebook = require('./lib/publishFacebook')
 const publishReddit = require('./lib/publishReddit')
+const {postToBlueSky} = require('./lib/publishBlueSky')
 
 module.exports = () => {
   const { expiryDate, discountCodes } = currentCoupons()
   const discount = randomDiscount()
   const coupon = discountCodes[discount + '%']
 
-  publishTweet(twitterStatus(discount, coupon, expiryDate))
-  publishFacebook(facebookPost(discount, coupon, expiryDate))
+  // publishTweet(twitterStatus(discount, coupon, expiryDate))
+  
+   // Post to BlueSky
+   postToBlueSky(`New coupon code: ${coupon} (valid until ${expiryDate})`)
+
   publishReddit(redditPost(discount, coupon, expiryDate), `New coupon code (valid until ${expiryDate})`)
 }
