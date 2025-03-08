b759660dcdf88ca4f1e738cc694d8a845311d6b2
Harsh Kumar <harshmastic@gmail.com>
2025-03-08 22:10:12 +0530

    migrate logger to Ts

---
+++ lib/logger.ts
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2019-2024 Bjoern Kimminich & the OWASP Juice Shop contributors.
+ * SPDX-License-Identifier: MIT
+ */
+
+import winston from 'winston'
+
+const logger = winston.createLogger({
+  transports: [
+    new winston.transports.Console({ level: process.env.NODE_ENV === 'test' ? 'error' : 'info' })
+  ],
+  format: winston.format.simple()
+})
+
+export default logger
