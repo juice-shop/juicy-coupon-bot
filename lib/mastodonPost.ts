fbd98a8d18ddf955b3c24c3608d66f43cb6116a8
Harsh Kumar <harshmastic@gmail.com>
2025-03-09 11:49:54 +0530

    mastodonPost migrated

---
+++ lib/mastodonPost.ts
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2019-2024 Bjoern Kimminich & the OWASP Juice Shop contributors.
+ * SPDX-License-Identifier: MIT
+ */
+
+import logger from "./logger.ts"
+import colors from 'colors'
+import { PostParameters } from './types/types.ts'
+import generateCouponMessage from './utils/generateCoupon.ts'
+
+const prepareMastodonPost = (params: PostParameters): string => {
+  const status = generateCouponMessage(params)
+  logger.info(`[${colors.green('✔')}] Mastodon post prepared: ${colors.cyan(status)}`)
+  return status;
+}
+
+export default prepareMastodonPost;
