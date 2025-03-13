a7be4033edef14a828aa81ca8bb120e11dc4f73f
Harsh Kumar <harshmastic@gmail.com>
2025-03-13 14:49:55 +0530

    replace sync-request with fetch

--- lib/currentCoupons.ts
+++ lib/currentCoupons.ts
@@ -5,14 +5,13 @@
 
 import logger from './logger.ts'
 import colors from 'colors'
-import request from 'sync-request'
 
-export default (apiEndpoint: string = 'https://5j4d1u7jhf.execute-api.eu-west-1.amazonaws.com/default/JuicyCouponFunc'): { expiryDate: string, discountCodes: Record<string, string> } => {
+export default async (apiEndpoint: string = 'https://5j4d1u7jhf.execute-api.eu-west-1.amazonaws.com/default/JuicyCouponFunc'): Promise<{ expiryDate: string, discountCodes: Record<string, string> }> => {
   try {
-    const res = (request as unknown as (method: string, url: string) => any)('GET', apiEndpoint)
-    const body = res.getBody()
-    logger.info(`[${colors.green('✔')}] API lookup success: ${body}`)
-    return JSON.parse(body)
+    const res = await fetch(apiEndpoint)
+    const body = await res.json()
+    logger.info(`[${colors.green('✔')}] API lookup success: ${JSON.stringify(body)}`)
+    return body
   } catch (error) {
     if (error instanceof Error) {
       logger.warn(`[${colors.red('❌')}] API lookup failed: ${colors.red(error.message)}`)
