a0d34f9bad02bb0e98be207114778a06a5019083
Harsh Kumar <harshmastic@gmail.com>
2025-03-15 02:18:54 +0530

    redditComment test file migrated

---
+++ test/redditComment.spec.ts
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2019-2024 Bjoern Kimminich & the OWASP Juice Shop contributors.
+ * SPDX-License-Identifier: MIT
+ */
+
+import { test } from "node:test";
+import assert from "node:assert";
+import redditComment from "../lib/redditPost.ts";
+import defaultParams from "./testUtils.ts";
+
+test("Reddit comment should always contain discount amount in bold text", () => {
+  for (let i = 10; i <= 40; i++) {
+    const customParams = { ...defaultParams, discount: i };
+    assert.ok(redditComment(customParams).includes(`**${i}%**`));
+  }
+});
+
+test("Reddit comment should always contain coupon code in code syntax", () => {
+  for (let i = 0; i < 100; i++) {
+    const customParams = { ...defaultParams, discount: i };
+    console.log("000000000000000000000000000000000000000000000000");
+    assert.ok(
+      redditComment(customParams).includes("`:COUPON:`"),
+      "Expected coupon code in code syntax: `:COUPON:`"
+    );
+  }
+});
+
+test("Reddit comment should always mention expiration date", () => {
+  for (let i = 0; i < 100; i++) {
+    const comment = redditComment(defaultParams);
+    console.log(`111111111111111111111111111111111111111111111111111`);
+    assert.ok(
+      comment.includes(":EXPIRATION:"),
+      "Expected expiration date: :EXPIRATION:"
+    );
+  }
+});
