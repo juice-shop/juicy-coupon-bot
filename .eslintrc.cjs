5f40b0cf79503dc802bb6cca7e53836a916954e5
Harsh Kumar <harshmastic@gmail.com>
2025-03-15 15:26:56 +0530

    add eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,44 @@
+module.exports = {
+  extends: [
+    'standard-with-typescript',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  plugins: ['@typescript-eslint'],
+  env: {
+    browser: true,
+    node: true,
+    jasmine: true,
+    mocha: true,
+    jest: true
+  },
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    project: './tsconfig.json'
+  },
+  ignorePatterns: [
+    '.eslintrc.js',
+    'build/**',
+    'node_modules/**',
+    '.nyc_output/**',
+    'dist/**'
+  ],
+  overrides: [
+    {
+      files: ['**/*.ts'],
+      rules: {
+        'no-void': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/strict-boolean-expressions': 'off',
+        '@typescript-eslint/no-var-requires': 'off',
+        '@typescript-eslint/no-misused-promises': 'off',
+        "quotes": ["error", "double"], 
+        "semi": ["error", "always"] 
+      }
+    }
+  ]
+};
